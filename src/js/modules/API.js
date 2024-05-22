467d02f6e9acd517f0c6421eea665ad7e28dfc6f
Thomas-Spb <thomas-spb@list.ru>
2024-05-22 07:29:34 +0300

    Add Api

--- src/js/modules/API.js
+++ src/js/modules/API.js
@@ -13,7 +13,7 @@ const formatQueryString = params => {
   return `${searchParams.toString()}`;
 };
 
-export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://chatter-waiting-path.glitch.me';
 
 export const fetchProducts = async (params = {}) => {
   //   console.log('params: ', params);
--- src/js/modules/CartElem.jsx
+++ src/js/modules/CartElem.jsx
@@ -1,29 +1,31 @@
 import { API_URL } from './API';
 import { cartStore } from './Store';
 
-export const CartElem = product => (
-  <li class="cart__item">
-    <img src={`${API_URL}${product.photoURl}`} alt={product.name} class="cart__img" />
-    <h4 class="cart__item_title">{product.name}</h4>
-    <div class="cart__counter">
-      <button
-        class="cart__counter_btn"
-        onClick={() => {
-          cartStore.postCart({ id: product.id, quantity: product.quantity - 1 });
-        }}
-      >
-        -
-      </button>
-      <input type="number" class="cart__counter_input" max="99" min="0" value={product.quantity} />
-      <button
-        class="cart__counter_btn"
-        onClick={() => {
-          cartStore.postCart({ id: product.id, quantity: product.quantity + 1 });
-        }}
-      >
-        +
-      </button>
-    </div>
-    <div class="cart__price">{product.price}&nbsp;₽</div>
-  </li>
-);
+export const CartElem = product => {
+  return (
+    <li class="cart__item">
+      <img src={`${API_URL}${product.photoUrl}`} alt={product.name} class="cart__img" />
+      <h4 class="cart__item_title">{product.name}</h4>
+      <div class="cart__counter">
+        <button
+          class="cart__counter_btn"
+          onClick={() => {
+            cartStore.postCart({ id: product.id, quantity: product.quantity - 1 });
+          }}
+        >
+          -
+        </button>
+        <input type="number" class="cart__counter_input" max="99" min="0" value={product.quantity} />
+        <button
+          class="cart__counter_btn"
+          onClick={() => {
+            cartStore.postCart({ id: product.id, quantity: product.quantity + 1 });
+          }}
+        >
+          +
+        </button>
+      </div>
+      <div class="cart__price">{product.price}&nbsp;₽</div>
+    </li>
+  );
+};
